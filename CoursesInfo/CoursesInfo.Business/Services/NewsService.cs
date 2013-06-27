d708413d275f582961d254901d3d142c687fa057
Igor Pischik <pischik.igor@gmail.com>
2013-06-27 20:06:43 +0300

    Add NewsService

---
+++ CoursesInfo/CoursesInfo.Business/Services/NewsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoursesInfo.Core.Entities;
+using CoursesInfo.Data;
+
+namespace CoursesInfo.Business.Services
+{
+    class NewsService:Service
+    {
+        private readonly IRepository<News> _newsRepository
+            = new NHibernateRepository<News>();
+
+        public IEnumerable<News> GetAllNews()
+        {
+            return _newsRepository.GetAll();
+        }
+    }
+}
